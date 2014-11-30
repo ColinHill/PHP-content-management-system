3184777ac80b48671fe980b7069f8907faded1f5
MFredericks <MitchellBFredericks@gmail.com>
2014-11-30 18:53:37 +0000

    user priv get/set

--- FinalProject/users.php
+++ FinalProject/users.php
@@ -1,5 +1,5 @@
 <?php
-class article
+class users
 {
     private $id;
     private $first_name;
@@ -11,6 +11,12 @@ class article
     private $creationDate;
     private $modifyBy;
     private $modifyDate;
+
+    private $privid;
+    private $admin;
+    private $editor;
+    private $author;
+
 
     public function __construct($record)
     {
@@ -24,7 +30,42 @@ class article
         $this->creationDate = $record['CreationDate'];
         $this->modifyBy = $record['ModifyBy'];
         $this->modifyDate = $record['ModifyDate'];
+
+        $this->privid = $record['Privileges_ID'];
+        $this->admin = $record['Administrator'];
+        $this->editor = $record['Editor'];
+        $this->author = $record['Author'];
     }//end constructor
+
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+
+    public function setAdmin($_admin)
+    {
+        $this->admin = $_admin;
+    }
+
+    public function getEditor()
+    {
+        return $this->editor;
+    }
+
+    public function setEditor($_editor)
+    {
+        $this->editor = $_editor;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($_author)
+    {
+        $this->author = $_author;
+    }
 
 }//end class
 

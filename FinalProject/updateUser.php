b4c1d472dbc9a2b4d7fcffc80df3ce79d52f5cde
MFredericks <MitchellBFredericks@gmail.com>
2014-12-01 17:43:37 +0000

    Update user fixed up a bit, default check needs work on updateUser.php

--- FinalProject/updateUser.php
+++ FinalProject/updateUser.php
@@ -1,3 +1,27 @@
+<?php
+include_once ("dbConnect.php");
+$db = dbConnect::getConnection();
+
+$userid = $_POST['update'];
+$query = "SELECT * FROM Users WHERE User_ID = '$userid'";
+$result = mysqli_query($db, $query);
+
+while ($row = mysqli_fetch_assoc($result)):
+$first_name = $row['first_name'];
+$last_name = $row['last_name'];
+$user_name = $row['user_name'];
+$password = $row['password'];
+$salt = $row['salt'];
+endwhile;
+
+$query = "SELECT * FROM Privileges WHERE Users_User_ID = '$userid'";
+$result = mysqli_query($db, $query);
+while ($row = mysqli_fetch_assoc($result)):
+$administrator = $row['Administrator'];
+$editor = $row['Editor'];
+$author = $row['Author'];
+endwhile;
+?>
 <html>
 <head>
 
@@ -10,22 +34,22 @@
 <form id="updateUser" method="post" action="runUpdateUser.php">
 
     <p>User ID:
-        <input id="User_ID" name="User_ID" type="text" maxlength="255" value="" readonly="readonly" />
+        <input id="User_ID" name="User_ID" type="text" maxlength="255" value="<?php echo $userid; ?>" readonly="readonly" />
 
     <p>First Name:
-        <input id="first_name" name="first_name" type="text" maxlength="255" value=""/>
+        <input id="first_name" name="first_name" type="text" maxlength="255" value="<?php echo $first_name; ?>"/>
 
     <p>Last Name:
-        <input id="last_name" name="last_name" type="text" maxlength="255" value=""/>
+        <input id="last_name" name="last_name" type="text" maxlength="255" value="<?php echo $last_name; ?>"/>
 
     <p>Username:
-        <input id="user_name" name="user_name" type="text" maxlength="255" value=""/>
+        <input id="user_name" name="user_name" type="text" maxlength="255" value="<?php echo $user_name; ?>"/>
 
     <p>Password:
-        <input id="password" name="password" type="text" maxlength="255" value=""/>
+        <input id="password" name="password" type="text" maxlength="255" value="<?php echo $password; ?>"/>
 
     <p>Salt:
-        <input id="salt" name="salt" type="text" maxlength="255" value=""/>
+        <input id="salt" name="salt" type="text" maxlength="255" value="<?php echo $salt; ?>"/>
 
     <p>Privileges:
 

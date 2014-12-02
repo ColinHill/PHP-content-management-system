db8c09a8053378c772d5c54473d4b2cac95e10d8
MFredericks <MitchellBFredericks@gmail.com>
2014-12-02 16:20:21 +0000

    CRUD on Site Pages done

--- FinalProject/updatePages.php
+++ FinalProject/updatePages.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inet2005
- * Date: 12/2/14
- * Time: 3:44 PM
- */ 
+include_once ('dbConnect.php');
+$db = dbConnect::getConnection();
+$siteid = $_POST['update'];
+
+$query = "SELECT * FROM SitePages WHERE SitePages_ID = '$siteid'";
+$result = mysqli_query($db, $query);
+
+while ($row = mysqli_fetch_assoc($result)):
+    $name = $row['Name'];
+    $alias = $row['Alias'];
+    $description = $row['Description'];
+
+endwhile;
+?>
+<html>
+<head>
+    <title>Update Site Page:</title>
+</head>
+
+<body>
+
+<form id="updatePage" method="post" action="runUpdatePage.php">
+
+    <p>Site Page ID:
+        <input id="SitePages_ID" name="SitePages_ID" type="text" maxlength="255" value="<?php echo $siteid; ?>" readonly="readonly" />
+
+    <p>Name:
+        <input id="Name" name="Name" type="text" maxlength="255" value="<?php echo $name; ?>"/>
+
+    <p>Alias:
+        <input id="Alias" name="Alias" type="text" maxlength="255" value="<?php echo $alias; ?>"/>
+
+    <p>Description:
+        <input id="Description" name="Description" type="text" size="100" maxlength="255" value="<?php echo $description; ?>"/>
+
+    <p><input id="submit" type="submit" name="submit" value="Update Site Page" />
+
+</body>
+</html>

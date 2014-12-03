881a6bc6a7a5eec3faca2077c9c93ae5da814930
[Colin Hill] <[niloc_321@hotmail.com]>
2014-12-03 00:33:09 +0000

    All navigation buttons and log in / log out added.

--- FinalProject/validation.js
+++ FinalProject/validation.js
@@ -15,12 +15,22 @@ function validateForm() {
         alert("Description must be filled out");
         return false;
     }
-    var x = document.forms["createPage"]["Description"].value;
+    var x = document.forms["updatePage"]["Name"].value;
+    if (x==null || x=="") {
+        alert("Name must be filled out");
+        return false;
+    }
+    var x = document.forms["updatePage"]["Alias"].value;
+    x = x.replace( /\s/g, "");
+    if (x==null || x=="") {
+        alert("Alias must be filled out");
+        return false;
+    }
+    var x = document.forms["updatePage"]["Description"].value;
     if (x==null || x=="") {
         alert("Description must be filled out");
         return false;
     }
-}
-
+}//end validateForm()
 
 
